29c451fbe6f7714c7072a92c81233997f2fa6a56
Molly James <molly.james@metoffice.gov.uk>
2024-08-21 21:13:09 +0100

    Fixing linting issues

--- tests/test_calculate_binding_energy.py
+++ tests/test_calculate_binding_energy.py
@@ -3,14 +3,16 @@ import binding_energy_solution.calculate_binding_energy as calculate_binding_ene
 
 
 def test_calc_pair_binding_energy():
-    """Test that function returns result for pair binding energy given by example in Technical Exercise"""
+    """Test that function returns result for pair binding energy given by
+    example in Technical Exercise"""
     r = 6.82e-10  # separation distance in m
     u = -1.0e-22  # binding energy in J
     assert calculate_binding_energy.calc_pair_binding_energy(r) == u
 
 
 def test_calc_trio_binding_energy():
-    """Test that function returns 3x the result for pair binding energy given by example in Technical Exercise"""
+    """Test that function returns 3x the result for pair binding energy given
+    by example in Technical Exercise"""
     r = 6.82e-10  # separation distance in m
     u = -1.0e-22  # binding energy in J
     assert calculate_binding_energy.calc_trio_binding_energy(r) == 3 * u
